579aef471c1a57abca4de33bb99b28bd4ca83546
Crazy <c919342864@yahoo.com>
2016-09-28 15:35:02 +0800

    add users

--- pages/personal/index/index.js
+++ pages/personal/index/index.js
@@ -5,17 +5,27 @@ var index_obj = require(root_path+'function/personal_index.js')
 
 Page({
   data:{
-    userInfo: {}
-    // text:"这是一个页面"
+    userInfo: {},
+    y_menus:[
+      {title:'消息通知'},
+      {title:'浏览历史'},
+      {title:'吐槽一下'},
+    ],
+    x_menus:[
+      {title:'收藏',icon:'/images/collection.png'},
+      {title:'夜间',icon:'/images/eve.png',classes:'two_side'},
+      {title:'设置',icon:'/images/set.png'},
+    ],
   },
   onLoad:function(options){
     var that = this;
     app.getUserInfo(function(userInfo){
-      console.log(userInfo);
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+        console.log(userInfo);
+        
+        //设置用户信息
+        that.setData({
+            userInfo:userInfo
+        })
     });
   },
   onReady:function(){
